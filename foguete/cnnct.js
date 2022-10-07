8d5d1ac892b9c456ed8149e8d072b06d4d567ab6
RodrigoSOS <doxrigor@gmail.com>
2022-10-07 17:36:46 -0300

    teste3

--- foguete/cnnct.js
+++ foguete/cnnct.js
@@ -27,9 +27,8 @@ script.onload = () => {
         // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
         // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
     }).then((unityInstance) => myGameInstance = unityInstance);
-    myGameInstance = unityInstance;
 }
-
+myGameInstance = unityInstance;
 var unityInstance = window.document.getElementById("unity-canvas");
 	
 function com(input){
