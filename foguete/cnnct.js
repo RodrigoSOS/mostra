cd83ce106a6ff0683f0f0e949160a48697758791
RodrigoSOS <doxrigor@gmail.com>
2022-10-07 17:47:07 -0300

    teste7

--- foguete/cnnct.js
+++ foguete/cnnct.js
@@ -1,19 +1,3 @@
-//var myGameInstance = null;
-//var unityInstance = window.unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%", {onProgress: UnityProgress});
-/*
-var myGameInstance = createUnityInstance(document.querySelector("#unity-canvas"), {
-        dataUrl: "Build/foguete.data",
-        frameworkUrl: "Build/foguete.framework.js",
-        codeUrl: "Build/foguete.wasm",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "Cnnct",
-        productName: "Empinar Moto",
-        productVersion: "1.0",
-        // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-        // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-    });
-*/
-
 var myGameInstance = null;
 {
     createUnityInstance(document.querySelector("#unity-canvas"), {
@@ -26,11 +10,10 @@ var myGameInstance = null;
         productVersion: "1.0",
         // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
         // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-    });
+    }).then((unityInstance) => myGameInstance = unityInstance);
     
 }
 myGameInstance = unityInstance;
-var unityInstance = window.document.getElementById("unity-canvas");
 	
 function com(input){
     myGameInstance.SendMessage('JSHook', 'console', input);
