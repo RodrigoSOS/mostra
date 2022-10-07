0e1ebf67fa71a9782451ac4610f409f2444ffaf5
RodrigoSOS <doxrigor@gmail.com>
2022-10-07 17:22:48 -0300

    teste1

--- foguete/cnnct.js
+++ foguete/cnnct.js
@@ -1,5 +1,6 @@
 //var myGameInstance = null;
 //var unityInstance = window.unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%", {onProgress: UnityProgress});
+/*
 var myGameInstance = createUnityInstance(document.querySelector("#unity-canvas"), {
         dataUrl: "Build/foguete.data",
         frameworkUrl: "Build/foguete.framework.js",
@@ -11,6 +12,23 @@ var myGameInstance = createUnityInstance(document.querySelector("#unity-canvas")
         // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
         // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
     });
+*/
+
+var myGameInstance = null;
+script.onload = () => {
+    createUnityInstance(document.querySelector("#unity-canvas"), {
+        dataUrl: "Build/foguete.data",
+        frameworkUrl: "Build/foguete.framework.js",
+        codeUrl: "Build/foguete.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "Cnnct",
+        productName: "Empinar Moto",
+        productVersion: "1.0",
+        // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+        // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+    }).then((unityInstance) => myGameInstance = unityInstance);
+    
+}
 
 var unityInstance = window.document.getElementById("unity-canvas");
 	
